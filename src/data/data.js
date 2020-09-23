5b4bba57e33648c49a35ffb85fc58f553336662a
LucasLaibly <original7245@gmail.com>
2020-09-22 21:00:25 -0500

    navbar

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,9 @@
-const HEADER = "HEADER"
+const HEADER=[{HEADER:"header"}];
 
-const BANNER = {
-    HEADING = "Lucas Laibly",
-    DESCRIPTION = "Description"
-};
+const NAV_BAR_DATA = [
+    { id: 1, url: "/", label: "home" },
+    { id: 2, url: "#intro", label: "intro" },
+    { id: 3, url: "#projects", label: "projects" },
+    { id: 4, url: "#library", label: "library" },
+    { id: 5, url: "#endorsements", label: "endorsements" }
+];
