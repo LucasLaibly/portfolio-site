c312bf75613bffd1fc5d25f0cbf910359df602a2
LucasLaibly <original7245@gmail.com>
2020-09-22 21:20:47 -0500

    navbar trim

--- src/data/data.js
+++ src/data/data.js
@@ -2,8 +2,6 @@ const HEADER=[{HEADER:"header"}];
 
 const NAV_BAR_DATA = [
     { id: 1, url: "/", label: "home" },
-    { id: 2, url: "#intro", label: "intro" },
-    { id: 3, url: "#projects", label: "projects" },
-    { id: 4, url: "#library", label: "library" },
-    { id: 5, url: "#endorsements", label: "endorsements" }
+    { id: 2, url: "#projects", label: "projects" },
+    { id: 3, url: "#library", label: "library" },
 ];
